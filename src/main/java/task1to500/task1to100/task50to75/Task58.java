906fe53e0a5209c3399b2cbae18cb3b325e30e93
tymanoff <djdkdancik@gmail.com>
2022-10-13 13:24:12 +0400

    add Task58

---
+++ src/main/java/task1to500/task1to100/task50to75/Task58.java
@@ -0,0 +1,37 @@
+package task1to500.task1to100.task50to75;
+
+public class Task58 {
+
+    /**
+     * Given a string s consisting of words and spaces, return the length of the last word in the string.
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: s = "Hello World"
+     * Output: 5
+     * Explanation: The last word is "World" with length 5.
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: s = "   fly me   to   the moon  "
+     * Output: 4
+     * Explanation: The last word is "moon" with length 4.
+     * <p>
+     * Example 3:
+     * <p>
+     * Input: s = "luffy is still joyboy"
+     * Output: 6
+     * Explanation: The last word is "joyboy" with length 6.
+     * <p>
+     * Constraints:
+     * 1 <= s.length <= 104
+     * s consists of only English letters and spaces ' '.
+     * There will be at least one word in s.
+     * */
+
+    public int lengthOfLastWord(String s) {
+        String[] stringArr = s.split(" ");
+        return stringArr[stringArr.length-1].length();
+    }
+}
