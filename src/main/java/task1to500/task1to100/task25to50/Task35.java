a6662e7afcc6c590b04c7b070630605f2f3a56e6
tymanoff <djdkdancik@gmail.com>
2022-10-14 22:20:42 +0400

    add Task35

---
+++ src/main/java/task1to500/task1to100/task25to50/Task35.java
@@ -0,0 +1,54 @@
+package task1to500.task1to100.task25to50;
+
+public class Task35 {
+
+    /**
+     * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     * You must write an algorithm with O(log n) runtime complexity.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [1,3,5,6], target = 5
+     * Output: 2
+     * <p>
+     * Example 2:
+     * <p>
+     * Input: nums = [1,3,5,6], target = 2
+     * Output: 1
+     * <p>
+     * Example 3:
+     * <p>
+     * Input: nums = [1,3,5,6], target = 7
+     * Output: 4
+     * <p>
+     * Constraints:
+     * 1 <= nums.length <= 104
+     * -104 <= nums[i] <= 104
+     * nums contains distinct values sorted in ascending order.
+     * -104 <= target <= 104
+     */
+
+    public int searchInsert(int[] nums, int target) {
+        int mid = nums.length / 2;
+        if (target == nums[mid]) {
+            return mid;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            mid = (end - start) / 2 + start;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
